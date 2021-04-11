ad1b09064ce7ea96437ebecf051d63fb25f23980
paulzay <paulotieno2@gmail.com>
2021-04-11 19:51:50 +0300

    build loading screen component and add styling

---
+++ src/components/Loading.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './loading.css';
+
+export default class Loader extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="container">
+            <div className="row">
+              <a className="intro-banner-vdo-play-btn blue-bg" target="_blank">
+                <i className="glyphicon glyphicon-play whiteText" aria-hidden="true" />
+                <span className="ripple blue-bg" />
+                <span className="ripple blue-bg" />
+                <span className="ripple blue-bg" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
