5989e3d293dec5a820c109a51f8bdf2dfa712c4b
paulzay <paulotieno2@gmail.com>
2021-04-12 15:04:43 +0300

    add tempscale reducer to rootreducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import weather from './weatherReducer';
+import tempScale from './toggleScaleReducer';
 
 const rootReducer = combineReducers({
     weather,
+    tempScale,
 });
 
 export default rootReducer;
