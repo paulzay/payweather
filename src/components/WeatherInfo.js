5bb33529f4092835d4b13ce937cb17e0c655881b
paulzay <paulotieno2@gmail.com>
2021-04-13 22:07:44 +0300

    implement pagination and apply loading screen

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,25 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
-
+import ReactPaginate from 'react-paginate';
 import { getWeather, toggleTempScale } from '../redux/actions/index';
 import Loading from './Loading';
 import { kelvinToCelsius, kelvinToFahrenheit } from './helpers';
 // materialui stuff
-import AppBar from '@material-ui/core/AppBar';
-import Button from '@material-ui/core/Button';
-// import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
 
 const useStyles = makeStyles((theme) => ({
     icon: {
@@ -52,79 +44,101 @@ const useStyles = makeStyles((theme) => ({
       padding: theme.spacing(6),
     },
   }));
-function WeatherInfo({weather, getWeather, tempScale, toggleTempScale}){
-    const classes = useStyles;
+function WeatherInfo({weather, getWeather, tempScale, toggleTempScale, loading}){
+  const classes = useStyles;
 
-    useEffect(() => {
-        getWeather();
-    }, [getWeather]);
+  const [offset, setOffset] = useState(0);
+  const [perPage] = useState(3);
+  const [pageCount, setPageCount] = useState(0)
 
-    // const isFetchingWeather = weather.isLoading;
-    const celsiusFontWeight = tempScale === 'celsius' ? 'bolder' : 'normal';
-    const fahrenheitFontWeight = tempScale === 'fahrenheit' ? 'bolder' : 'normal';
-  
-    const onToggleTempScale = () => {
-      toggleTempScale();
-    };
-    const temperature = (temp) => {
-      return tempScale === 'celsius' ? kelvinToCelsius(temp) : kelvinToFahrenheit(temp);
-    };
+  useEffect(() => {
+      getWeather();
+      setPageCount(Math.ceil(weather.length / perPage))
+  }, [getWeather]);
 
+  const onToggleTempScale = () => {
+    toggleTempScale();
+  };
+  const temperature = (temp) => {
+    return tempScale === 'celsius' ? kelvinToCelsius(temp) : kelvinToFahrenheit(temp);
+  };
 
-        return(
-          <>
-              <div className="align-center">
-                <label className="switch">
-                  <input
-                    onChange={onToggleTempScale}
-                    // checked={props.degreeFormat}
-                    type="checkbox"
-                  ></input>
-                  <span className="slider round align-center">
-                    <div
-                      style={{
-                        display: "flex",
-                        gap: "20px",
-                        color: "black",
-                        zIndex: 2,
-                        position: "relative",
-                        fontSize: "12px",
-                      }}
-                    >
-                      <span>C</span>
-                      <span>F</span>
-                    </div>
-                  </span>
-                </label>
-              </div>
-            <div className={classes.heroContent}>
-                <Container className={classes.cardGrid} maxWidth="md">
-                    <Grid container spacing={4}>
-                        {weather.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card className={classes.card}>
-                                    <CardContent className={classes.cardContent}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Temp: <br/>{temperature(card.main.temp)}
-                                        </Typography>
-                                        <Typography>
-                                            Date: <br/>{card.dt_txt}
-                                        </Typography>
-                                        <Typography>
-                                          <p>
-                                            Feels like: {temperature(card.main.feels_like)} | {card.weather[0].description} |
-                                            Humidity: {card.main.humidity}%
-                                          </p>
-                                        </Typography>
-                                    </CardContent>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-            </div> 
-          </>    
-        )
+  const handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    setOffset(selectedPage + 1)
+  };
+  const slice = weather.slice(offset, offset + perPage);
+  if(!loading){
+  return(
+    <>
+      <div className={classes.heroContent}>
+          <Container className={classes.cardGrid} maxWidth="md">
+          <div className="align-center">
+        <label className="switch">
+          <input
+            onChange={onToggleTempScale}
+            // checked={props.degreeFormat}
+            type="checkbox"
+          ></input>
+          <span className="slider round align-center">
+            <div
+              style={{
+                display: "flex",
+                gap: "20px",
+                color: "black",
+                zIndex: 2,
+                position: "relative",
+                fontSize: "12px",
+              }}
+            >
+              <span>C</span>
+              <span>F</span>
+            </div>
+          </span>
+        </label>
+      </div>
+      <ReactPaginate
+        previousLabel={"prev"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}/>
+    
+              <Grid container spacing={4}>
+                  {slice.map((card) => (
+                      <Grid item key={card} xs={12} sm={6} md={4}>
+                          <Card className={classes.card} key={card.dt}>
+                              <CardContent className={classes.cardContent}>
+                                  <Typography gutterBottom variant="h5" component="h2">
+                                      Temp: <br/>{temperature(card.main.temp)}
+                                  </Typography>
+                                  <Typography>
+                                      Date: <br/>{card.dt_txt}
+                                  </Typography>
+                                  <Typography>
+                                    <p>
+                                      Feels like: {temperature(card.main.feels_like)} | {card.weather[0].description} |
+                                      Humidity: {card.main.humidity}%
+                                    </p>
+                                  </Typography>
+                              </CardContent>
+                          </Card>
+                      </Grid>
+                  ))}
+              </Grid>
+          </Container>
+      </div> 
+    </>    
+  )
+  }else{
+    return <Loading />
+  }
     
 
 }
@@ -136,6 +150,7 @@ WeatherInfo.propTypes = {
 
 const mapStateToProps = state => ({
     weather: state.weather.weather,
+    loading: state.weather.isLoading,
     tempScale: state.tempScale
 });
 
