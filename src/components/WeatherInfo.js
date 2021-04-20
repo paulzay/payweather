89effa5def33f13856fefa6b307acfcafd4b2794
paulzay <paulotieno2@gmail.com>
2021-04-21 00:24:43 +0300

    finalize slider and data conversion for store

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types'
-import ReactPaginate from 'react-paginate';
+import Carousel from 'nuka-carousel';
 import { getWeather, toggleTempScale } from '../redux/actions/index';
 import Loading from './Loading';
 import { kelvinToCelsius, kelvinToFahrenheit } from './helpers';
@@ -21,9 +21,6 @@ const useStyles = makeStyles((theme) => ({
       backgroundColor: theme.palette.background.paper,
       padding: theme.spacing(8, 0, 6),
     },
-    heroButtons: {
-      marginTop: theme.spacing(4),
-    },
     cardGrid: {
       paddingTop: theme.spacing(8),
       paddingBottom: theme.spacing(8),
@@ -33,130 +30,100 @@ const useStyles = makeStyles((theme) => ({
       display: 'flex',
       flexDirection: 'column',
     },
-    cardMedia: {
-      paddingTop: '56.25%', // 16:9
-    },
     cardContent: {
       flexGrow: 1,
     },
-    footer: {
-      backgroundColor: theme.palette.background.paper,
-      padding: theme.spacing(6),
-    },
+    carousel: {
+      marginTop: theme.spacing(4)
+    }
   }));
 function WeatherInfo({weather, getWeather, tempScale, toggleTempScale, loading}){
   const classes = useStyles;
 
-  const [offset, setOffset] = useState(0);
-  const [perPage] = useState(3);
-  const [pageCount, setPageCount] = useState(0)
-
   useEffect(() => {
-      getWeather();
-      setPageCount(Math.ceil(weather.length / perPage))
+    getWeather();
   }, [getWeather]);
 
   const onToggleTempScale = () => {
     toggleTempScale();
   };
   const temperature = (temp) => {
-    return tempScale === 'celsius' ? kelvinToCelsius(temp) : kelvinToFahrenheit(temp);
+    return tempScale === 'fahrenheit' ? kelvinToFahrenheit(temp) : kelvinToCelsius(temp);
   };
 
-  const handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    setOffset(selectedPage + 1)
-  };
-  const slice = weather.slice(offset, offset + perPage);
   if(!loading){
   return(
     <>
       <div className={classes.heroContent}>
-          <Container className={classes.cardGrid} maxWidth="md">
+        <Container className={classes.cardGrid} maxWidth="md">
           <div className="align-center">
-        <label className="switch">
-          <input
-            onChange={onToggleTempScale}
-            // checked={props.degreeFormat}
-            type="checkbox"
-          ></input>
-          <span className="slider round align-center">
-            <div
-              style={{
-                display: "flex",
-                gap: "20px",
-                color: "black",
-                zIndex: 2,
-                position: "relative",
-                fontSize: "12px",
-              }}
-            >
-              <span>C</span>
-              <span>F</span>
-            </div>
-          </span>
-        </label>
-      </div>
-      <ReactPaginate
-        previousLabel={"prev"}
-        nextLabel={"next"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={pageCount}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={5}
-        onPageChange={handlePageClick}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}/>
-    
-              <Grid container spacing={4}>
-                  {slice.map((card) => (
-                      <Grid item key={card} xs={12} sm={6} md={4}>
-                          <Card className={classes.card} key={card.dt}>
-                              <CardContent className={classes.cardContent}>
-                                  <Typography gutterBottom variant="h5" component="h2">
-                                      Temp: <br/>{temperature(card.main.temp)}
-                                  </Typography>
-                                  <Typography>
-                                      Date: <br/>{card.dt_txt}
-                                  </Typography>
-                                  <Typography>
-                                    <p>
-                                      Feels like: {temperature(card.main.feels_like)} | {card.weather[0].description} |
-                                      Humidity: {card.main.humidity}%
-                                    </p>
-                                  </Typography>
-                              </CardContent>
-                          </Card>
-                      </Grid>
-                  ))}
-              </Grid>
-          </Container>
+            <label className="switch">
+              <input
+                onChange={onToggleTempScale}
+                type="checkbox"
+              ></input>
+              <span className="slider round align-center">
+                <div
+                  style={{
+                    display: "flex",
+                    gap: "20px",
+                    color: "black",
+                    zIndex: 2,
+                    position: "relative",
+                    fontSize: "12px",
+                  }}
+                >
+                  <span>F</span>
+                  <span>C</span>
+                </div>
+              </span>
+            </label>
+          </div>
+          <Carousel slidesToShow={3} cellSpacing={20} slidesToScroll={2} className={classes.carousel}>
+            {Object.values(weather).map((card) => (
+              <Grid item key={card}>
+                <Card className={classes.card} key={card.date}>
+                  <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                          Temp: <br/>{temperature(card.temp)}
+                      </Typography>
+                      <Typography>
+                          Date: <br/>{card.date}
+                      </Typography>
+                      <Typography>
+                        <p>
+                          Humidity: {card.humidity}% | Feels Like: {temperature(card.feels_like)} | Pressure: {card.pressure} 
+                        </p>
+                      </Typography>
+                  </CardContent>
+                </Card>      
+              </Grid>             
+            ))}
+          </Carousel>
+        </Container>
       </div> 
     </>    
   )
   }else{
     return <Loading />
   }
-    
-
 }
 
 WeatherInfo.propTypes = {
-    weather: PropTypes.instanceOf(Object).isRequired,
-    getWeather: PropTypes.func.isRequired, 
+  weather: PropTypes.instanceOf(Object).isRequired,
+  getWeather: PropTypes.func.isRequired,
+  toggleTempScale: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
-    weather: state.weather.weather,
-    loading: state.weather.isLoading,
-    tempScale: state.tempScale
+  weather: state.weather.weather,
+  loading: state.weather.isLoading,
+  tempScale: state.tempScale
 });
 
 const mapDispatchToProps = dispatch => ({
-    getWeather: () => dispatch(getWeather()),
-    toggleTempScale: () => dispatch(toggleTempScale())
+  getWeather: () => dispatch(getWeather()),
+  toggleTempScale: () => dispatch(toggleTempScale())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(WeatherInfo);;
--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,7 +1,48 @@
+import moment from 'moment';
+
 export const kelvinToCelsius = (kelvin) => {
     return `${Math.round(kelvin - 273.15)}°C`;
 };
 export const kelvinToFahrenheit = (kelvin) => {
     return `${Math.round((kelvin - 273.15) * (9 / 5) + 32)}°F`;
 };
-  
+
+const averages = (list, property = 'temp') => {
+  const len = list.length;
+  const sum = list.reduce((acc, cur) => {
+    return acc + cur.main[property];
+  }, 0);
+  const avg = sum / len;
+  const res = +avg.toFixed(2);
+  return res;
+};
+
+export const dataConversion = (list) => {
+  const copyList = [...list];
+  const mappedHoursList = copyList.map(
+    (item) => item['dt_txt'].split(' ')[0]
+  );
+  const objOfUniqueDays = [...new Set(mappedHoursList)].reduce((acc, cur) => {
+    return { ...acc, [cur]: [] };
+  }, {});
+  copyList.forEach((item) => {
+    const exactDayDate = item['dt_txt'].split(' ')[0];
+    objOfUniqueDays[exactDayDate] = [
+      ...objOfUniqueDays[exactDayDate],
+      { ...item, main: { ...item.main, temp: +item.main.temp.toFixed(2) } }
+    ];
+  });
+
+  for (const prop in objOfUniqueDays) {
+    objOfUniqueDays[prop] = {
+      arr: objOfUniqueDays[prop],
+      date: moment(prop).format('DD, MMM YY'),
+      temp: averages(objOfUniqueDays[prop]),
+      feels_like: averages(objOfUniqueDays[prop], 'feels_like'),
+      pressure: averages(objOfUniqueDays[prop], 'pressure'),
+      humidity: averages(objOfUniqueDays[prop], 'humidity'),
+    };
+  }
+
+  return objOfUniqueDays;
+};
